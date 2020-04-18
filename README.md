3094c56702c18b418f13f77571eec9a3d5064451
Michał <mwargan@gmail.com>
2020-04-18 12:46:51 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Download the script
+
+Use `python3 testrss.py` in Terminal or a similar app
---
+++ testrss.py
@@ -0,0 +1,3 @@
+from theparser import TheParser
+
+rssobject=WhizRssAggregator("https://wordpress.org/support/view/no-replies/feed/")
